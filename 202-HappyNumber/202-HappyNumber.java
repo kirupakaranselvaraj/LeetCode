24ae748c5df38e4f36381755015561609580b602
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2026-07-09 09:11:27 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:11:23
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> seen = new HashSet<>();
+        while (n != 1 && !seen.contains(n)) {
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+    private int getNext(int n) {
+        int total = 0;
+        while (n > 0) {
+            int d = n % 10;
+            total += d * d;
+            n /= 10;
+        }
+        return total;
+    }
+}
