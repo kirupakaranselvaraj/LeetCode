794c093fc32fc374389b797250a8d10321841d9b
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2026-07-09 09:10:48 +0530

    Create 1528-KidsWithTheGreatestNumberOfCandies

---
+++ 1528-KidsWithTheGreatestNumberOfCandies/1528-KidsWithTheGreatestNumberOfCandies.py
@@ -0,0 +1,13 @@
+# Last updated: 09/07/2026, 09:10:43
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        max_candies = max(candies)
+        result = []
+        for c in candies:
+            result.append(c + extraCandies >= max_candies)
+        return result
