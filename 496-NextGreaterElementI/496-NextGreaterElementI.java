26fafdbab6fda279dc5a2ea1f87175687368c519
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2026-07-09 09:11:05 +0530

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/496-NextGreaterElementI.java
@@ -0,0 +1,21 @@
+// Last updated: 09/07/2026, 09:11:00
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreater = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                nextGreater.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreater.getOrDefault(nums1[i], -1);
+        }
+        
+        return result;
+    }
+}
