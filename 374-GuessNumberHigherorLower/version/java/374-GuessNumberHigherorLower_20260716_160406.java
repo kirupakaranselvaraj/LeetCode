d08c3a8ffa38d6eef4382aebbceba353a4ce8603
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2026-07-16 16:04:10 +0530

    Create 374-GuessNumberHigherorLower

---
+++ 374-GuessNumberHigherorLower/version/java/374-GuessNumberHigherorLower_20260716_160406.java
@@ -0,0 +1,16 @@
+// Last updated: 16/07/2026, 16:04:06
+1class Solution {
+2    public int fib(int n) {
+3        if (n <= 1) {
+4            return n;
+5        }
+6        int first = 0;
+7        int second = 1;
+8        for (int i = 2; i <= n; i++) {
+9            int third = first + second;
+10            first = second;
+11            second = third;
+12        }
+13        return second;
+14    }
+15}
