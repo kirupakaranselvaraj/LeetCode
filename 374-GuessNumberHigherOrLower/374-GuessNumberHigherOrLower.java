896b9d67300e5f2ff5ac1b8c93db15e4947d1a3f
KIRUPAKARAN S <kirupakaran.s151205@gmail.com>
2026-07-28 14:41:13 +0530

    Create 374-GuessNumberHigherOrLower

---
+++ 374-GuessNumberHigherOrLower/374-GuessNumberHigherOrLower.java
@@ -0,0 +1,20 @@
+// Last updated: 28/07/2026, 14:41:12
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left = 1;
+        int right = n;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int result = guess(mid);
+            if (result == 0) {
+                return mid;
+            }
+            if (result == 1) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
